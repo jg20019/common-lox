f44332045da5d03e15a649f944984c9f1b37f9a3
John Gibson <jg20019@gmail.com>
2023-07-14 11:14:09 -0400

    feat: Always create macro
    I think this rule means only evaluate at compile time so the while
    macro was acting up again. It seems like eval-when is used to make sure
    macros that rely on functions exist when the macro runs at compile time.
    
    Since the while macro relied on standard Common Lisp, placing it in an
    eval when was unnecessary

--- macros.lisp
+++ macros.lisp
@@ -1,5 +1,5 @@
 (in-package #:common-lox)
 
-(eval-when (:compile-toplevel) 
-  (defmacro while (predicate &body body)
-    `(loop while ,predicate do (progn ,@body))))
+
+(defmacro while (predicate &body body)
+  `(loop while ,predicate do (progn ,@body)))
